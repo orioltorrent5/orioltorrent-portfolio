abd9d4ce9d5532479d0ea0cc8dcad963e31e8014
orioltorrent5 <otorrent@inforolot.com>
2024-12-17 19:18:59 +0100

    first version

---
+++ addons/script.js
@@ -0,0 +1,6 @@
+// Añade un efecto al enviar el formulario
+document.getElementById('contact-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('¡Gracias por contactarme! Pronto recibirás una respuesta.');
+  });
+  
